0850b838b2158784362eedb8b90e3120ce7b8681
Alex0ar <arnautalexandru2@gmail.com>
2026-07-07 16:28:19 +0300

    stage 3 complete

--- File Server/task/src/client/Main.java
+++ File Server/task/src/client/Main.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Main {
     private static final String address = "127.0.0.1";
@@ -16,13 +17,61 @@ public class Main {
                 Socket socket = new Socket(address, port);
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                Scanner userInput = new Scanner(System.in);
                 ) {
-            System.out.println("Client started!");
-            out.println("Give me everything you have!");
-            System.out.println("Sent: Give me everything you have!");
+            System.out.print("Enter action (1 - get a file, 2 - create a file, 3 - delete a file): ");
+            String action = userInput.nextLine();
+            if (action.equals("exit")) {
+                out.println(action);
+                System.out.println("The request was sent.");
+                return;
+            }
+            System.out.print("Enter filename: ");
+            String filename = userInput.nextLine();
+            String fileContent = null;
+            if (action.equals("2")) {
+                System.out.print("Enter file content: ");
+                fileContent = userInput.nextLine();
+            }
+            System.out.println("The request was sent.");
 
-            String received = in.readLine();
-            System.out.println("Received: " + received);
+            String request = null;
+            switch (action) {
+                case "1" -> request = "GET " + filename;
+                case "2" -> request = "PUT " + filename + " " + fileContent;
+                case "3" -> request = "DELETE " + filename;
+            }
+
+            //System.out.println("request: " + request);
+
+            out.println(request);
+
+            String response = in.readLine();
+            System.out.println("[client] response from server: " + response);
+            switch (action) {
+                case "1" -> {
+                    String[] responseArray = response.split(" ", 2);
+                    if (responseArray[0].equals("200")) {
+                        System.out.println("The content of the file is: " + responseArray[1]);
+                    } else {
+                        System.out.println("The response says that the file was not found!");
+                    }
+                }
+                case "2" -> {
+                    if (response.equals("200")) {
+                        System.out.println("The response says that the file was created!");
+                    } else {
+                        System.out.println("The response says that creating the file was forbidden!");
+                    }
+                }
+                case "3" -> {
+                    if (response.equals("200")) {
+                        System.out.println("The response says that the file was successfully deleted!");
+                    } else {
+                        System.out.println("The response says that the file was not found!");
+                    }
+                }
+            }
 
         } catch (UnknownHostException e) {
             throw new RuntimeException(e);
--- File Server/task/src/server/Controller.java
+++ File Server/task/src/server/Controller.java
@@ -8,31 +8,78 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.Buffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Controller {
     private static final String address = "127.0.0.1";
     private static final int port = 23456;
+    //private static final Path DATA_DIRECTORY = Paths.get("File Server", "task", "src", "server", "data");
+    private static final Path DATA_DIRECTORY = Paths.get("src", "server", "data");
+
     FileManager fileManager = new FileManager();
 
     public void startServer() {
         try (ServerSocket serverSocket = new ServerSocket(port, 50, InetAddress.getByName(address))) {
             System.out.println("Server started!");
-            //while (true) {
-            try (
-                    Socket socket = serverSocket.accept();
-                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                    ) {
-                String request = in.readLine();
-                System.out.println("Received: " + request);
+            while (true) {
+                try (
+                        Socket socket = serverSocket.accept();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                        ) {
+                    String request = in.readLine();
+                    if (request.equals("exit")) {
+                        break;
+                    }
+                    String response = "";
 
-                out.println("All files were sent!");
-                System.out.println("Sent: " + "All files were sent!");
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+                    // TO DELETE
+                    //System.out.println("request: " + request);
+
+                    String[] requestArray = request.split(" ", 3);
+
+                    // TO DELETE
+//                    for (int i = 0; i < requestArray.length; i++) {
+//                        System.out.print(requestArray[i] + " | 2");
+//                    }
+
+                    switch (requestArray[0]) {
+                        case "GET" -> {
+                            Path filePath = DATA_DIRECTORY.resolve(requestArray[1]);
+                            if (Files.exists(filePath)) {
+                                response = "200 " + Files.readString(filePath);
+                            } else {
+                                response = "404";
+                            }
+                        }
+                        case "PUT" -> {
+                            Path filePath = DATA_DIRECTORY.resolve(requestArray[1]);
+                            if (Files.exists(filePath)) {
+                                response = "403";
+                            } else {
+                                response = "200";
+                                Files.writeString(filePath, requestArray[2]);
+                            }
+                        }
+                        case "DELETE" -> {
+                            Path filePath = DATA_DIRECTORY.resolve(requestArray[1]);
+                            if (Files.exists(filePath)) {
+                                Files.delete(filePath);
+                                response = "200";
+                            } else {
+                                response = "404";
+                            }
+                        }
+                    }
+                    out.println(response);
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             }
-            //}
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
