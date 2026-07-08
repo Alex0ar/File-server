b34b8072312aa03e5428d91cbff84e7e2d3f81d9
Alex0ar <arnautalexandru2@gmail.com>
2026-07-08 16:28:48 +0300

    stage 4 incomplete

--- File Server/task/src/client/Main.java
+++ File Server/task/src/client/Main.java
@@ -1,77 +1,111 @@
 package client;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main {
     private static final String address = "127.0.0.1";
     private static final int port = 23456;
+    private static final Path DATA_DIRECTORY = Paths.get(System.getProperty("user.dir"), "File Server", "task", "src", "client", "data");
+
+//    private enum FileSelectionMethod {
+//        BY_ID,
+//        BY_NAME
+//    }
 
     public static void main(String[] args) {
         try (
                 Socket socket = new Socket(address, port);
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                DataInputStream in = new DataInputStream(socket.getInputStream());
                 Scanner userInput = new Scanner(System.in);
                 ) {
             System.out.print("Enter action (1 - get a file, 2 - create a file, 3 - delete a file): ");
             String action = userInput.nextLine();
             if (action.equals("exit")) {
-                out.println(action);
+                out.writeUTF(action);
                 System.out.println("The request was sent.");
                 return;
             }
-            System.out.print("Enter filename: ");
-            String filename = userInput.nextLine();
-            String fileContent = null;
-            if (action.equals("2")) {
-                System.out.print("Enter file content: ");
-                fileContent = userInput.nextLine();
-            }
-            System.out.println("The request was sent.");
 
             String request = null;
             switch (action) {
-                case "1" -> request = "GET " + filename;
-                case "2" -> request = "PUT " + filename + " " + fileContent;
-                case "3" -> request = "DELETE " + filename;
-            }
-
-            //System.out.println("request: " + request);
-
-            out.println(request);
-
-            String response = in.readLine();
-            System.out.println("[client] response from server: " + response);
-            switch (action) {
                 case "1" -> {
-                    String[] responseArray = response.split(" ", 2);
-                    if (responseArray[0].equals("200")) {
-                        System.out.println("The content of the file is: " + responseArray[1]);
+                    String fileToDownload = null;
+                    System.out.print("Do you want to get the file by name or by id (1 - name, 2 - id): ");
+                    switch (userInput.nextLine()) {
+                        case "1" -> {
+                            System.out.print("Enter name: ");
+                            fileToDownload = userInput.nextLine();
+                        }
+                        case "2" -> {
+                            System.out.print("Enter id: ");
+                            fileToDownload = userInput.nextLine();
+                        }
+                    }
+                    request = "GET " + fileToDownload;
+                    out.writeUTF(request);
+                    System.out.println("The request was sent.");
+
+                    // response from server structure: STATUS_CODE | FILE_LENGTH | FILE IN BYTES
+
+                    int STATUS_CODE = in.readInt();
+                    if  (STATUS_CODE == 200) {
+                        int fileContentLength = in.readInt();
+                        byte[] fileContent = new byte[fileContentLength];
+                        in.readFully(fileContent, 0, fileContentLength);
+                        System.out.print("The file was downloaded! Specify a name for it: ");
+                        String fileNameToCreate = userInput.nextLine();
+                        Files.write(DATA_DIRECTORY.resolve(fileNameToCreate),  fileContent);
+                        System.out.println("File saved on the hard drive!");
                     } else {
                         System.out.println("The response says that the file was not found!");
                     }
+
                 }
                 case "2" -> {
-                    if (response.equals("200")) {
-                        System.out.println("The response says that the file was created!");
-                    } else {
-                        System.out.println("The response says that creating the file was forbidden!");
-                    }
+                    System.out.print("Enter name of the file: ");
+                    String fileNameToSend = userInput.nextLine();
+                    System.out.print("Enter name of the file to be saved on server: ");
+                    String fileNameToSave = userInput.nextLine();
+
                 }
                 case "3" -> {
-                    if (response.equals("200")) {
-                        System.out.println("The response says that the file was successfully deleted!");
-                    } else {
-                        System.out.println("The response says that the file was not found!");
-                    }
+
                 }
             }
+
+//            String response = in.readLine();
+//            System.out.println("[client] response from server: " + response);
+//            switch (action) {
+//                case "1" -> {
+//                    String[] responseArray = response.split(" ", 2);
+//                    if (responseArray[0].equals("200")) {
+//                        System.out.println("The content of the file is: " + responseArray[1]);
+//                    } else {
+//                        System.out.println("The response says that the file was not found!");
+//                    }
+//                }
+//                case "2" -> {
+//                    if (response.equals("200")) {
+//                        System.out.println("The response says that the file was created!");
+//                    } else {
+//                        System.out.println("The response says that creating the file was forbidden!");
+//                    }
+//                }
+//                case "3" -> {
+//                    if (response.equals("200")) {
+//                        System.out.println("The response says that the file was successfully deleted!");
+//                    } else {
+//                        System.out.println("The response says that the file was not found!");
+//                    }
+//                }
+//            }
 
         } catch (UnknownHostException e) {
             throw new RuntimeException(e);
